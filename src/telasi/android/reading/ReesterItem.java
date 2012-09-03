c9b8963e9c0ef31667d7d5e917e6a39f10beec8b
Dimitri Kurashvili <dimitri@c12.ge>
2012-09-03 15:08:34 +0400

    ReesterItem class

---
+++ src/telasi/android/reading/ReesterItem.java
@@ -0,0 +1,50 @@
+package telasi.android.reading;
+
+public class ReesterItem {
+	private int id;
+	private int routeKey;
+	private int scheduleKey;
+	private int sequence;
+	private Account account;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRouteKey() {
+		return routeKey;
+	}
+
+	public void setRouteKey(int routeKey) {
+		this.routeKey = routeKey;
+	}
+
+	public int getScheduleKey() {
+		return scheduleKey;
+	}
+
+	public void setScheduleKey(int scheduleKey) {
+		this.scheduleKey = scheduleKey;
+	}
+
+	public int getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(int sequence) {
+		this.sequence = sequence;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+}
