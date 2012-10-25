7c7bb762a8448cf7c8eb7f9b77680e842da1fe54
Dimitri Kurashvili <dimitri@c12.ge>
2012-10-25 14:13:49 +0400

    testing reester new parameter savings

--- test/telasi/android/reading/model/ReadingControllerTest.java
+++ test/telasi/android/reading/model/ReadingControllerTest.java
@@ -1,7 +1,5 @@
 package telasi.android.reading.model;
 
-import java.util.List;
-
 import junit.framework.TestCase;
 
 public class ReadingControllerTest extends TestCase {
@@ -9,11 +7,21 @@ public class ReadingControllerTest extends TestCase {
   public void testSendReester() throws Exception {
     String username = "dimitri";
     String password = "dima123";
-    int inspectorId = 11795;
-    List<Reester> reester = ReadingController.getReestersOverHTTP(username, password, inspectorId, 1);
-    assertNotNull(reester);
-    // route name
-    String rtname = reester.get(0).getRouteName();
-    assertNotNull(rtname);
+    // int inspectorId = 11795;
+    // getting reester
+    Reester reester = ReadingController.getReesterOverHTTP(username, password, 96);
+    // update reester & send it back
+    ReesterItem item = reester.getItems()[0];
+    item.getMeter().setNewCoeff(100);
+    item.getMeter().setNewNumber("666666");
+    item.getMeter().setNewSealNumber("7777777");
+    ReadingController.sendReesterOverHTTP(reester, username, password);
+    // getting reester again
+    reester = ReadingController.getReesterOverHTTP(username, password, 96);
+    item = reester.getItems()[0];
+    assertEquals(100, item.getMeter().getNewCoeff().intValue());
+    assertEquals("666666", item.getMeter().getNewNumber());
+    assertEquals("7777777", item.getMeter().getNewSealNumber());
   }
+
 }
